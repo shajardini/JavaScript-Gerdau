6d0b41467a86210b9356b0505da44a5a61d29524
shajardini <sha.jardini@gmail.com>
2023-11-23 16:59:16 -0300

    first commit

---
+++ AULA1/script.js
@@ -0,0 +1,30 @@
+// //comentário de 1 linha
+
+// /* comentário de +
+
+// de uma linha */
+
+// //    RECURSOS NATIVOS DO JAVASCRIPT
+
+// console.log('Boa tarde!')//imprimir texto no console.
+// console.info('Hoje é quinta-feira!')
+// console.warn('Cuidado!')
+// console.error('Mensagem de erro.')
+
+// // Caixas de diálogo:
+
+// alert('Janela de alerta.')
+// confirm('Você pode clicar em ok ou cancelar.')//Janla de confirmação
+// prompt('Qual o seu nome?')//Janela para obter alguma informação do usuário
+
+//Variável - espaço na memória para armazenar alguma informação
+//let, var, const
+
+var numero = 1
+let nome1 = 'Aline'
+const pi = 3.14
+
+let nome = prompt('Digite seu nome: ')
+// console.log(nome)
+
+console.log('Seja bem vindo(a) ' + nome + ' a nossa aula!')
---
+++ exercício/script.js
@@ -0,0 +1,36 @@
+// let numero = Number(prompt('Digite um número: '))
+
+// console.log(numero)
+
+// // - pedir pro usuário : Digite outro número
+
+// // - Pegar o numero e guardar em uma variável
+// let numero2 = Number(prompt('Digite outro número: '))
+// // - Mostrar no console
+
+// console.log(numero2)
+
+// alert(numero + numero2)
+
+// + - soma
+// - subtração
+// Divisão /
+// * multiplicação
+// % - resto da divisão
+// ** Exponenciação 
+
+
+// Crie uma variável para armazenar a temperatura em graus Celsius
+// Converta a temperatura para graus Fahrenheit
+// Exiba a temperatura em janela de alerta
+// (0 °C × 9/5) + 32 = 32 °F
+
+// 1. Digita(prompt)- °C
+// 2. c Guardar o valor em uma variável
+// 3. c * 9/5 + 32
+
+let grausC = prompt('Digite a temperatura em °C:')
+
+let fahrenheit = (grausC * 9/5) + 32
+
+alert(fahrenheit + '°F')
